95ffb8f4ca672e4bcdf850dbb3739c344fda129b
RoyalPrince777 <nosunnolife123@gmail.com>
2026-08-25 16:39:47 +0100

    Mount Signal trust before ranking

--- ownpost_store/gates.py
+++ ownpost_store/gates.py
@@ -13,6 +13,7 @@ from oap_ride_admin import register_oap_ride_admin
 from movement_hub import register_movement_hub
 from oap_language import register_oap_language
 from signal_pulse_controls import register_signal_pulse_controls
+from signal_trust import register_signal_trust
 from signal_intelligence import register_signal_intelligence
 from oap_checkpoints import register_checkpoints
 from provider_contracts import register_provider_contracts
@@ -23,45 +24,25 @@ from regulated_rails import register_regulated_rails
 from oap_observability import register_observability
 from oap_finalization import register_finalization
 
-if 'spot_family' not in app.blueprints:
-    register_spot_family(app, db, uid)
-if 'royal_oap' not in app.blueprints:
-    register_royal_oap(app, db, uid)
-if 'oap_intelligence' not in app.blueprints:
-    register_oap_intelligence(app, db, uid)
-if 'youth_real_education' not in app.blueprints:
-    register_youth_real_education(app, db, uid)
-if 'oap_youth_club' not in app.blueprints:
-    register_youth_club(app, db, uid)
-if 'bank_intelligence' not in app.blueprints:
-    register_bank_intelligence(app, db, uid)
-if 'background_258' not in app.blueprints:
-    register_background_258(app, db, uid)
-if 'oap_ride' not in app.blueprints:
-    register_oap_ride(app, db, uid)
-if 'oap_ride_admin' not in app.blueprints:
-    register_oap_ride_admin(app, db, uid)
-if 'oap_movement_hub' not in app.blueprints:
-    register_movement_hub(app, db, uid)
-if 'oap_language' not in app.blueprints:
-    register_oap_language(app, db, uid)
-if 'oap_signal_pulse_controls' not in app.blueprints:
-    register_signal_pulse_controls(app, db, uid)
-if 'oap_signal_intelligence' not in app.blueprints:
-    register_signal_intelligence(app, db)
-if 'oap_checkpoints' not in app.blueprints:
-    register_checkpoints(app, db, uid)
-if 'oap_provider_contracts' not in app.blueprints:
-    register_provider_contracts(app)
-if 'oap_provider_adapters' not in app.blueprints:
-    register_provider_adapters(app)
-if 'oap_location_bridge' not in app.blueprints:
-    register_location_bridge(app)
-if 'oap_event_bridge' not in app.blueprints:
-    register_event_bridge(app, db, uid)
-if 'oap_regulated_rails' not in app.blueprints:
-    register_regulated_rails(app)
-if 'oap_observability' not in app.blueprints:
-    register_observability(app, db, uid)
-if 'oap_finalization' not in app.blueprints:
-    register_finalization(app, db, uid)
+if 'spot_family' not in app.blueprints: register_spot_family(app, db, uid)
+if 'royal_oap' not in app.blueprints: register_royal_oap(app, db, uid)
+if 'oap_intelligence' not in app.blueprints: register_oap_intelligence(app, db, uid)
+if 'youth_real_education' not in app.blueprints: register_youth_real_education(app, db, uid)
+if 'oap_youth_club' not in app.blueprints: register_youth_club(app, db, uid)
+if 'bank_intelligence' not in app.blueprints: register_bank_intelligence(app, db, uid)
+if 'background_258' not in app.blueprints: register_background_258(app, db, uid)
+if 'oap_ride' not in app.blueprints: register_oap_ride(app, db, uid)
+if 'oap_ride_admin' not in app.blueprints: register_oap_ride_admin(app, db, uid)
+if 'oap_movement_hub' not in app.blueprints: register_movement_hub(app, db, uid)
+if 'oap_language' not in app.blueprints: register_oap_language(app, db, uid)
+if 'oap_signal_pulse_controls' not in app.blueprints: register_signal_pulse_controls(app, db, uid)
+if 'oap_signal_trust' not in app.blueprints: register_signal_trust(app, db, uid)
+if 'oap_signal_intelligence' not in app.blueprints: register_signal_intelligence(app, db)
+if 'oap_checkpoints' not in app.blueprints: register_checkpoints(app, db, uid)
+if 'oap_provider_contracts' not in app.blueprints: register_provider_contracts(app)
+if 'oap_provider_adapters' not in app.blueprints: register_provider_adapters(app)
+if 'oap_location_bridge' not in app.blueprints: register_location_bridge(app)
+if 'oap_event_bridge' not in app.blueprints: register_event_bridge(app, db, uid)
+if 'oap_regulated_rails' not in app.blueprints: register_regulated_rails(app)
+if 'oap_observability' not in app.blueprints: register_observability(app, db, uid)
+if 'oap_finalization' not in app.blueprints: register_finalization(app, db, uid)
