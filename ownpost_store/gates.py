41e89b5fed6f3e6016e15c4b4d51fcde1abdd899
RoyalPrince777 <nosunnolife123@gmail.com>
2026-08-25 14:30:34 +0100

    Mount unified regulated rails boundary

--- ownpost_store/gates.py
+++ ownpost_store/gates.py
@@ -13,6 +13,7 @@ from movement_hub import register_movement_hub
 from oap_language import register_oap_language
 from oap_checkpoints import register_checkpoints
 from provider_contracts import register_provider_contracts
+from regulated_rails import register_regulated_rails
 from oap_finalization import register_finalization
 
 if 'spot_family' not in app.blueprints:
@@ -39,5 +40,7 @@ if 'oap_checkpoints' not in app.blueprints:
     register_checkpoints(app, db, uid)
 if 'oap_provider_contracts' not in app.blueprints:
     register_provider_contracts(app)
+if 'oap_regulated_rails' not in app.blueprints:
+    register_regulated_rails(app)
 if 'oap_finalization' not in app.blueprints:
     register_finalization(app, db, uid)
