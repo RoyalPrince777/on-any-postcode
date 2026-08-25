8ad492763e66e42b32d3fc0fa1c4a6459ebb90f4
RoyalPrince777 <nosunnolife123@gmail.com>
2026-08-25 08:51:27 +0100

    Mount Bank Intelligence in live runtime

--- ownpost_store/gates.py
+++ ownpost_store/gates.py
@@ -5,6 +5,7 @@ from spot_family import register_spot_family
 from royal_oap import register_royal_oap
 from oap_intelligence import register_oap_intelligence
 from youth_real_education import register_youth_real_education
+from bank_intelligence import register_bank_intelligence
 from oap_finalization import register_finalization
 
 if 'spot_family' not in app.blueprints:
@@ -15,5 +16,7 @@ if 'oap_intelligence' not in app.blueprints:
     register_oap_intelligence(app, db, uid)
 if 'youth_real_education' not in app.blueprints:
     register_youth_real_education(app, db, uid)
+if 'bank_intelligence' not in app.blueprints:
+    register_bank_intelligence(app, db, uid)
 if 'oap_finalization' not in app.blueprints:
     register_finalization(app, db, uid)
