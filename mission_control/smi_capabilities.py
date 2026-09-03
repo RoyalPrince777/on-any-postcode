2cdc20efd3e34b4129af12ebe470ee4012b01b01
RoyalPrince777 <nosunnolife123@gmail.com>
2026-09-04 00:58:32 +0100

    Fix SMI capability import ordering

--- mission_control/smi_capabilities.py
+++ mission_control/smi_capabilities.py
@@ -11,7 +11,12 @@ from typing import Any
 
 from oap.smi.agi_core import AGICore
 
-from . import earth_intelligence, language_intelligence, life_intelligence, movement_intelligence
+from . import (
+    earth_intelligence,
+    language_intelligence,
+    life_intelligence,
+    movement_intelligence,
+)
 from .agents import INTELLIGENCE_WORLDS
 
 CROSS_SYSTEM_CAPABILITIES: tuple[dict[str, str], ...] = (
