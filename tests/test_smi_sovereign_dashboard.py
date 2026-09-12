c043f310b33052de7c297c2540af342373923d35
RoyalPrince777 <nosunnolife123@gmail.com>
2026-09-12 20:27:54 +0100

    Normalize dashboard test import spacing

--- tests/test_smi_sovereign_dashboard.py
+++ tests/test_smi_sovereign_dashboard.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-
 ROOT = Path(__file__).resolve().parents[1]
 WRAPPER = ROOT / "mission_control" / "templates" / "ollama_chat.html"
 DASHBOARD_JS = ROOT / "mission_control" / "static" / "smi_sovereign_dashboard.js"
