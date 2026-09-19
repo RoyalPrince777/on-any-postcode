4c7a414d6ae59a45cb7d56df38141730b1136ab7
RoyalPrince777 <nosunnolife123@gmail.com>
2026-09-19 04:23:58 +0100

    Restore Link message sync module wiring

--- mission_control/__init__.py
+++ mission_control/__init__.py
@@ -27,6 +27,7 @@ def init_app(app: Flask) -> None:
         hrm_durable_receipt,
         link_activity,
         link_call_audit,
+        link_message_sync,
         link_ping,
         link_presence,
         link_relationships,
