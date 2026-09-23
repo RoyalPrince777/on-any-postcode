972041a9cf2ae8ab969f63d42686f94ca098e4cb
RoyalPrince777 <nosunnolife123@gmail.com>
2026-09-23 10:44:22 +0100

    Fix Ruff import-block spacing in retry regressions

--- tests/test_movement_booking_idempotency.py
+++ tests/test_movement_booking_idempotency.py
@@ -8,7 +8,6 @@ import pytest
 
 from mission_control import movement_operations, routing, web_security
 
-
 MEMBER = "11111111-1111-4111-8111-111111111111"
 BOOKING = "22222222-2222-4222-8222-222222222222"
 OTHER_BOOKING = "33333333-3333-4333-8333-333333333333"
