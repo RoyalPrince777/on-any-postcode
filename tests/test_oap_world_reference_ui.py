d1267a67001a3b764b1fe4744d29a3d64d62dfb0
RoyalPrince777 <nosunnolife123@gmail.com>
2026-09-26 13:39:40 +0100

    Atlas UI tests: lock reference naming

--- tests/test_oap_world_reference_ui.py
+++ tests/test_oap_world_reference_ui.py
@@ -10,7 +10,7 @@ def test_oap_world_reference_ui_contract():
         "OAP WORLD",
         "STRIP OF NOISE",
         "Search the world... any postcode...",
-        "Map Intelligence",
+        "OAP Atlas",
         "The Spot",
         "The Link",
         "Market",
