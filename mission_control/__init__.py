6b71f0f6148e0ff736c5c3a3b61809736228da3c
RoyalPrince777 <nosunnolife123@gmail.com>
2026-09-26 14:43:06 +0100

    SIKA Global: register authenticated app surface

--- mission_control/__init__.py
+++ mission_control/__init__.py
@@ -86,6 +86,7 @@ def init_app(app: Flask) -> None:
     from .oap_library_views import bp as oap_library_bp
     from .on_any_place_routes import bp as on_any_place_bp
     from .product_core_views import bp as product_core_bp
+    from .sika_global_views import bp as sika_global_bp
     from .provider_views import bp as provider_bp
     from .travel_supply_views import bp as travel_supply_bp
     from .views import bp
@@ -1126,6 +1127,7 @@ def init_app(app: Flask) -> None:
 
     surface_security.register(app)
     app.register_blueprint(oap_library_bp)
+    app.register_blueprint(sika_global_bp)
     app.register_blueprint(music_public_bp)
     app.register_blueprint(on_any_place_bp)
     app.register_blueprint(membership_revenue_bp)
